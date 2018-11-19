bf023dbf12a7a6982df74016748034bf954ee169
Xin Zhao <xinz@mellanox.com>
2018-11-19 15:22:50 -0800

    fix on osc_win_free

--- ompi/mca/osc/ucx/osc_ucx_component.c
+++ ompi/mca/osc/ucx/osc_ucx_component.c
@@ -563,14 +563,15 @@ int ompi_osc_ucx_free(struct ompi_win_t *win) {
     OBJ_DESTRUCT(&module->outstanding_locks);
     OBJ_DESTRUCT(&module->pending_posts);
 
+    opal_common_ucx_mem_flush(module->mem, OPAL_COMMON_UCX_SCOPE_WORKER, 0);
+
+    DBG_OUT("ompi_osc_ucx_free: after mem_flush, mem = %p lock flag = %d\n",
+            (void *)module->mem, (int)module->state.lock);
+
     while (module->state.lock != TARGET_LOCK_UNLOCKED) {
         // not sure if this is required
         // ucp_worker_progress(mca_osc_ucx_component.ucp_worker);
     }
-
-    opal_common_ucx_mem_flush(module->mem, OPAL_COMMON_UCX_SCOPE_WORKER, 0);
-
-    DBG_OUT("ompi_osc_ucx_free: after mem_flush, mem = %p\n", (void *)module->mem);
 
     ret = module->comm->c_coll->coll_barrier(module->comm,
                                              module->comm->c_coll->coll_barrier_module);
